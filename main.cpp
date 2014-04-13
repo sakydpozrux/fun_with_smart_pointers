a9689fe7ac8811023131c9981910de195a541775
Szymon Koper <sakydpozrux@gmail.com>
2014-04-13 20:43:17 +0200

    Maybe now it looks quite better...

--- main.cpp
+++ main.cpp
@@ -9,16 +9,20 @@ namespace Example
     std::cout << "Example of boost::scoped_ptr :" << std::endl;
 
     boost::scoped_ptr<int> i(new int);
-    std::cout << "1. " << *i << " (new initializes int with default 0)" << std::endl;
+    std::cout << "1. " << *i << std::endl;
+    // 1. 0
 
     *i = 123;
     std::cout << "2. " << *i << std::endl;
+    // 2. 123
 
     *i.get() = 789; // Note that it is not i->get() = ...
     std::cout << "3. " << *i << std::endl;
+    // 3. 789
 
     i.reset(new int);
-    std::cout << "4. " << *i << " (again value is changed to default 0)" << std::endl;
+    std::cout << "4. " << *i << std::endl;
+    // 4. 0
 
     std::cout << "boost::scoped_ptr deletes itself when quitting scope" << std::endl;
 
@@ -33,6 +37,8 @@ int main()
 
   Example::scoped_ptr();
 
-  std::cout << "Now you can check for memory leaks and there should not be any." << std::endl;
+  std::cout << "Now you can check memory leaks:" << std::endl;
+  std::cout << "valgrind --tool=memcheck --quiet ./main" << std::endl;
+  std::cout << "There shouldn't be any. :)" << std::endl;
 }
 
